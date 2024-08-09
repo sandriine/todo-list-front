918983866bab44c1244da3fb44e1d05a2d55322a
sandriine <neveusandrine@free.fr>
2024-08-09 11:24:11 +0200

    TodoLists

---
+++ src/pages/TodoLists.tsx
@@ -0,0 +1,12 @@
+export function TodoList() {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        <li>Learn React</li>
+        <li>Learn TypeScript</li>
+        <li>Learn Angular</li>
+      </ul>
+    </div>
+  )
+}
