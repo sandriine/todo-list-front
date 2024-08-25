0571a33a1693577c70b3db2486a1a1898314bebf
DSanoussy <sanoussy.dansso@stonal.com>
2024-08-25 16:22:29 +0200

    mockserver - installing cors

--- mockserver/config/fastify.config.ts
+++ mockserver/config/fastify.config.ts
@@ -1,9 +1,13 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
 
 export const createServer = () => {
     const fastify = Fastify({ logger: true });
 
     // Add any Fastify plugins or custom settings here, like CORS or static file serving
+    fastify.register(cors, {
+        origin: '*',
+    })
 
     return fastify;
 };
