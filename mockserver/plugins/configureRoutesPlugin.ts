2fb888724d9f414126fc5bcdbde81f8c0481d260
DSanoussy <sanoussy.dansso@stonal.com>
2024-08-26 21:47:41 +0200

    refactoring plugin

--- mockserver/plugins/configureRoutesPlugin.ts
+++ mockserver/plugins/configureRoutesPlugin.ts
@@ -1,9 +1,10 @@
 // @ts-ignore
 import fp from 'fastify-plugin';
-import { FastifyInstance, FastifyPluginOptions } from 'fastify';
+import {FastifyInstance, FastifyPluginOptions} from 'fastify';
 import fs from 'fs';
 import path from 'path';
 import config from '../config/mockserver.config';
+import {RouteConfig} from '../config/types/interfaces';
 
 const configureRoutesPlugin = async (fastify: FastifyInstance, _: FastifyPluginOptions) => {
     fastify.log.info('[INFO] Running route configuration script...');
@@ -12,7 +13,6 @@ const configureRoutesPlugin = async (fastify: FastifyInstance, _: FastifyPluginO
     const files = fs.readdirSync(dataDir);
 
     files.forEach((file) => {
-        // Skip non-JSON files, such as README.md
         if (path.extname(file) !== '.json') {
             fastify.log.info(`[INFO] Skipping non-JSON file: ${file}`);
             return;
@@ -20,46 +20,53 @@ const configureRoutesPlugin = async (fastify: FastifyInstance, _: FastifyPluginO
 
         if (!config.routeConfig[file]) {
             fastify.log.info(`[INFO] Found new file: ${file}`);
+            addDefaultRouteConfig(file);
+        }
+    });
 
-            // Default configuration: only GET method without specific routes
-            config.routeConfig[file] = {
-                routes: ['GET'],
-                parents: [],
-                customParentKeys: {},
-                hasSpecificRoute: false,
-            };
+    fastify.log.info('[INFO] Configuration complete!');
+};
 
-            // Sort the routeConfig by file name
-            const sortedRouteConfig = Object.keys(config.routeConfig)
-                .sort()
-                .reduce((acc, key) => {
-                    acc[key] = config.routeConfig[key];
-                    return acc;
-                }, {} as typeof config.routeConfig);
+const addDefaultRouteConfig = (file: string) => {
+    config.routeConfig[file] = {
+        routes: ['GET'],
+        parents: [],
+        customParentKeys: {},
+        hasSpecificRoute: false,
+    };
 
-            const configString = `import {MockServerConfig} from "./types/interfaces";
-    
+    writeConfigToFile();
+};
+
+const writeConfigToFile = () => {
+    const sortedRouteConfig = Object.keys(config.routeConfig)
+        .sort()
+        .reduce((acc, key) => {
+            acc[key] = config.routeConfig[key];
+            return acc;
+        }, {} as typeof config.routeConfig);
+
+    const configString = generateConfigString(sortedRouteConfig);
+    fs.writeFileSync(path.join(__dirname, '../config/mockserver.config.ts'), configString, 'utf-8');
+};
+
+const generateConfigString = (sortedRouteConfig: { [key: string]: RouteConfig }): string => {
+    return `import {MockServerConfig} from "./types/interfaces";
+
 const config: MockServerConfig = {
     dataDir: '${config.dataDir}', // Directory where JSON files are stored
     routeConfig: {
         ${Object.entries(sortedRouteConfig).map(([key, value]) => `
         '${key}': {
             routes: [${value.routes.map(route => `'${route}'`).join(', ')}],
-            parents: ${value.parents ? `'${value.parents}'` : '[]'},
-            customParentKeys: ${value.customParentKeys ? `'${value.customParentKeys}'` : '{}'},
+            parents: ${JSON.stringify(value.parents)},
+            customParentKeys: ${JSON.stringify(value.customParentKeys)},
             hasSpecificRoute: ${value.hasSpecificRoute}
         }`).join(',')}
     }
 };
 
 export default config;`;
-
-            fs.writeFileSync(path.join(__dirname, '../config/mockserver.config.ts'), configString, 'utf-8');
-            fastify.log.info(`[INFO] Added default GET route configuration for ${file}`);
-        }
-    });
-
-    fastify.log.info('[INFO] Configuration complete!');
 };
 
 export default fp(configureRoutesPlugin);
