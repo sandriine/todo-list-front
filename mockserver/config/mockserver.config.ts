9754fc2e39c70bb755028358dfb201f956d03d83
DSanoussy <sanoussy.dansso@stonal.com>
2024-08-30 15:35:42 +0200

    Add items and fetch them on click

--- mockserver/config/mockserver.config.ts
+++ mockserver/config/mockserver.config.ts
@@ -3,34 +3,10 @@ import {MockServerConfig} from "./types/interfaces";
 const config: MockServerConfig = {
     dataDir: '../data', // Directory where JSON files are stored
     routeConfig: {
-        
-        'categories.json': {
-            routes: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-            parents: ['products'],
-            customParentKeys: {'products': 'productId'},
-            hasSpecificRoute: true
-        },
+
         'items.json': {
             routes: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
             parents: ['todos'],
-            customParentKeys: {},
-            hasSpecificRoute: true
-        },
-        'nothing.json': {
-            routes: ['GET'],
-            parents: [],
-            customParentKeys: {},
-            hasSpecificRoute: false
-        },
-        'products.json': {
-            routes: ['GET'],
-            parents: [],
-            customParentKeys: {},
-            hasSpecificRoute: false
-        },
-        'subtasks.json': {
-            routes: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-            parents: ['todos', 'items'],
             customParentKeys: {},
             hasSpecificRoute: true
         },
